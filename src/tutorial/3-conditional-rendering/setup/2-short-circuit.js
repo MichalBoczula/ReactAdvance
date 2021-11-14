f80003bcf2f6d92e1c63c385ea622b1c10372efd
Michal Boczula <michal.boczula0101@gmail.com>
2021-11-14 17:04:57 +0100

    added short-circuit

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
-// short-circuit evaluation
-// ternary operator
+import React, { useState } from "react";
+// short-circuit evaluation for diffrent value
+// ternary operator for diffrent value and tags
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("Majk");
+  const [isError, setIsError] = useState(false);
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <h1>{text || "john doe"}</h1>
+      {/* <h1>{text ? text : "john doe"}</h1> */}
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      <h2>{isError ? "there is error" : "there is no error"}</h2>
+    </>
+  );
 };
 
 export default ShortCircuit;
