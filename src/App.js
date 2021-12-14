68b39a7b205321f5dbea75381d6e48505b9a5b40
Michal Boczula <michal.boczula0101@gmail.com>
2021-12-14 22:28:38 +0100

    context exercise

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropDrilling from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
+import PropDrilling from "./tutorial/8-useContext/setup/1-context-api";
 
 function App() {
   return (
